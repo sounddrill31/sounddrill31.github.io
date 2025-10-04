dcfb0fccd0d9381697ff605c33338016037b13f1
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-10-04 22:02:45 +0000

    Minify why page as well

--- prepare-deploy.sh
+++ prepare-deploy.sh
@@ -7,6 +7,7 @@ mkdir -p out/js
 # Minify HTML files
 minify -o out/index.html index.html
 minify -o out/about.html about.html
+minify -o out/why.html why.html
 
 # Minify CSS files
 minify -o out/css/main.css css/main.css
@@ -15,4 +16,5 @@ minify -o out/css/scroll-indicator.css css/scroll-indicator.css
 # Minify JS files
 minify -o out/js/scroll-indicator.js js/scroll-indicator.js
 
+cp assets/* out/assets/
 echo "Minified files are ready in the 'out' directory."
